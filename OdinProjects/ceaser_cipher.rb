64c5992bcb7803ee9722708e8bbe3f36f2f86c32
pagnew96 <padraica30@gmail.com>
2024-11-25 01:18:20 +0000

    fixed bug on the cipher suite
    added to_i to the shift to make sure its a int

--- OdinProjects/ceaser_cipher.rb
+++ OdinProjects/ceaser_cipher.rb
@@ -1,8 +1,8 @@
 def caesar_cipher(message, shift)
   encrypted_message = message.chars.map do |char|
-    if char.match(/[a-zA-Z]/)  
-      base = char.match(/[a-z]/) ? 'a'.ord : 'A'.ord  
-      shifted_char = ((char.ord - base + shift) % 26 + base).chr
+    if char.match(/[a-zA-Z]/)
+      base = char.match(/[a-z]/) ? 'a'.ord : 'A'.ord
+      shifted_char = ((char.ord - base + shift.to_i) % 26 + base).chr
       shifted_char
     else
       char
@@ -11,10 +11,10 @@ def caesar_cipher(message, shift)
   encrypted_message.join
 end
 
-print "Hey! Enter the secret message you want to encrypt: " 
+print 'Hey! Enter the secret message you want to encrypt: '
 secret_message = gets.chomp
 
-print "And the key you want to shift by: "
+print 'And the key you want to shift by: '
 message_shift = gets.chomp
 
-puts ceaser_cipher(secret_message, message_shift)
+puts caesar_cipher(secret_message, message_shift)
